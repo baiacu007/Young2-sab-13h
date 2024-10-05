787f6c8a8ab15e2325270f040c9505fde7cad492
baia <aulahtml8@gmail.com>
2024-10-05 15:32:11 -0300

    aula 10

---
+++ aula 10/script.js
@@ -0,0 +1,46 @@
+const QUESTOS_QUIZ = [
+{
+    questao:"1+1 é?",
+    a: "2",
+    b: "11",
+    c: "0",
+    d: "3",
+    correta: b,
+
+    questao:"9+9 é?",
+    a: "99",
+    b: "18",
+    c: "10",
+    d: "73",
+    correta: a,
+
+    questao:"1+21 é?",
+    a: "12",
+    b: "22",
+    c: "121",
+    d: "20",
+    correta: c
+
+
+
+
+
+
+
+
+}
+
+]
+
+const QUIZ_TITULO = document.querySelector("h1")
+
+const ALTERNATIVA_A = document.querySelector('#texto_a')
+const ALTERNATIVA_B = document.querySelector('#texto_b')
+const ALTERNATIVA_C = document.querySelector('#texto_c')
+const ALTERNATIVA_D = document.querySelector('#texto_d')
+const BOTAO = document.querySelector('button')
+const QUIZ_CONTAINER = document.querySelector('.container')
+const ALTERNATIVAS = document.querySelectorAll('.resposta')
+
+
+
